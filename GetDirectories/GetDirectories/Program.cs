b214b3c5ff5c162a71d3950da7a7c029d3f92639
kumochan <thaotrinhminh@gmail.com>
2018-03-29 22:54:53 +0700

    init

---
+++ GetDirectories/GetDirectories/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDirectories
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please input directory: ");
+            string directory = Console.ReadLine();
+            Console.WriteLine("\nResult:");
+            if (Directory.Exists(directory))
+                GetHierarchicalFolderList(directory, "*");
+            else
+                Console.WriteLine("Not Exist!");
+
+            Console.ReadLine();
+        }
+
+        private static void GetHierarchicalFolderList(string path, string searchPattern = "*")
+        {
+            var folders = SearchHandler.GetDirectories(path);
+            string folder = string.Empty;
+            foreach (var item in folders)
+            {
+                Console.WriteLine(item);
+                folder = item;
+                GetHierarchicalChildItems(ref folder, searchPattern, 1);
+            }
+        }
+
+        private static void GetHierarchicalChildItems(ref string folder, string searchPattern, int level)
+        {
+            List<string> files = SearchHandler.GetFiles(folder, searchPattern);
+            if (files.Count <= 0) return;
+
+            var indent = string.Empty;
+            var splash = string.Empty;
+            for (var i = 0; i < level; i++)
+            {
+                indent += "---";
+                splash += "|  ";
+            }
+
+            foreach (var file in files)
+                Console.WriteLine(splash + indent + file);
+
+            List<string> folders = SearchHandler.GetDirectories(folder);
+            if (folders.Count > 0)
+            {
+                string subFolder = string.Empty;
+                foreach (var item in folders)
+                {
+                    Console.WriteLine(splash + item);
+                    subFolder = item;
+                    GetHierarchicalChildItems(ref subFolder, searchPattern, level + 1);
+                }
+            }
+        }
+    }
+}
---
+++ GetDirectories/GetDirectories/SearchHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GetDirectories
+{
+    class SearchHandler
+    {
+        public static List<string> GetFiles(string path, string pattern)
+        {
+            try
+            {
+                var listFiles = new List<string>();
+                DirectoryInfo directory = new DirectoryInfo(path);
+                FileInfo[] Files = directory.GetFiles(pattern);
+                foreach (FileInfo file in Files)
+                {
+                    listFiles.Add(file.Name);
+                }
+                return listFiles;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return new List<string>();
+            }
+        }
+
+        public static List<string> GetDirectories(string path)
+        {
+            try
+            {
+                return Directory.GetDirectories(path).ToList();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
